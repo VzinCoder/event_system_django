586c2b9822007168b76414a17c0467d275cc1f83
VzinCoder <contato.viniciusp.b@gmail.com>
2025-01-30 13:39:36 -0300

    edit view home_page

--- event_system_django/views.py
+++ event_system_django/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import get_object_or_404, render,redirect
 
 
 def home_page(request):
+    if request.user.is_authenticated:
+        return redirect('events')
     return render(request,'home.html')
