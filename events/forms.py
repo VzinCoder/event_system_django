0d317b8fcce69c465f07950cea82e5058cc213d7
VzinCoder <contato.viniciusp.b@gmail.com>
2025-02-03 16:41:12 -0300

    added field timezone

--- events/forms.py
+++ events/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from .models import Event
 from django.utils import timezone
-
+from zoneinfo import ZoneInfo
 class EventForm(forms.ModelForm):
+    timezone = forms.CharField(widget=forms.HiddenInput(),required=True)
     class Meta:
         model = Event
         fields = ['name', 'description', 'image', 'location', 'event_start_date', 'event_end_date', 
@@ -37,13 +38,46 @@ class EventForm(forms.ModelForm):
             'image': forms.FileInput(attrs={'class': 'form-control', 'required': False})
         }
 
+    def convert_data_to_utc(self, dt):
+        if not dt:
+            return None  # Retorna None se a data não for fornecida
+        
+        timezone_str = self.data.get('timezone')
+        if not timezone_str:
+            return dt  # Retorna a data original se o fuso horário não for fornecido
+        
+        try:
+            tz_user = ZoneInfo(timezone_str)
+            dt = dt.replace(tzinfo=tz_user)
+            return dt.astimezone(ZoneInfo("UTC"))
+        except Exception:
+            self.add_error('timezone', "Fuso horário inválido.")  # Adiciona um erro no formulário
+            return dt  # Retorna a data original para evitar falhas
+
+    def clean_event_start_date(self):
+        event_start_date = self.cleaned_data.get('event_start_date')
+        return self.convert_data_to_utc(event_start_date)
+
+    def clean_event_end_date(self):
+        event_end_date = self.cleaned_data.get('event_end_date')
+        return self.convert_data_to_utc(event_end_date)
+
+    def clean_registration_start_date(self):
+        registration_start_date = self.cleaned_data.get('registration_start_date')
+        return self.convert_data_to_utc(registration_start_date)
+
+    def clean_registration_end_date(self):
+        registration_end_date = self.cleaned_data.get('registration_end_date')
+        return self.convert_data_to_utc(registration_end_date)
+    
     def clean(self):
         cleaned_data = super().clean()
+        
         event_start_date = cleaned_data.get('event_start_date')
         event_end_date = cleaned_data.get('event_end_date')
         registration_start_date = cleaned_data.get('registration_start_date')
         registration_end_date = cleaned_data.get('registration_end_date')
-
+        
         # Validação da data de início das inscrições
         if registration_start_date and registration_start_date < timezone.now():
             self.add_error('registration_start_date', "A data de início das inscrições não pode estar no passado.")
